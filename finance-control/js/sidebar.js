d59601cd01f91cc87ae5e15292db9a9b41a6bd04
Eduard Philip <eduardophilip95@gmail.com>
2021-05-26 12:19:07 +0100

    sidebar structure and style

---
+++ finance-control/js/sidebar.js
@@ -0,0 +1,32 @@
+const sidebar = document.querySelector('.sidebar');
+const headerBtn = document.querySelector('.header__btn');
+const icon = document.querySelector('.sidebar-year__icon');
+const list = document.querySelector('.sidebar__list--dropdown');
+
+const yearLink = document.querySelectorAll('.sidebar-year__link')
+        /* .forEach(item => item.addEventListener('click', () => {
+            list.classList.toggle('sidebar__list--dropdown');
+            icon.classList.toggle('sidebar-year__icon--rotate')
+        })) */
+
+
+const toggleSidebar = () => {
+    if(headerBtn.classList.contains('header__btn--clicked')) {
+        sidebar.classList.remove('sidebar--active')
+        headerBtn.classList.remove('header__btn--clicked')
+    }else {
+        sidebar.classList.add('sidebar--active')
+        headerBtn.classList.add('header__btn--clicked')
+    }
+}
+
+headerBtn.addEventListener('click', toggleSidebar)
+
+for(let item of yearLink) {
+    console.log(item)
+    
+    item.onclick = () => {
+        list.classList.toggle('sidebar__list--dropdown');
+        icon.classList.toggle('sidebar-year__icon--rotate')
+    }
+}
