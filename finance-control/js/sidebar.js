ee13a949f969c946c63090a6735d96f0ab775564
Eduard Philip <eduardophilip95@gmail.com>
2021-06-14 21:56:54 +0100

    dealing with Click In Month

--- finance-control/js/sidebar.js
+++ finance-control/js/sidebar.js
@@ -1,8 +1,9 @@
 const sidebar = document.querySelector('.sidebar');
 const headerBtn = document.querySelector('.header__btn');
+const yearLink = document.querySelectorAll('.sidebar-year__link');
+const month = document.querySelectorAll('.sidebar-month__link');
 
 
-const yearLink = document.querySelectorAll('.sidebar-year__link')
 
 const toggleSidebar = () => {
     if(headerBtn.classList.contains('header__btn--clicked')) {
@@ -14,15 +15,33 @@ const toggleSidebar = () => {
     }
 }
 
-headerBtn.addEventListener('click', toggleSidebar)
-
-for(let item of yearLink) {
-    const parent = item.closest('.sidebar-year');
-    const icon = parent.querySelector('.sidebar-year__icon');
-    const list = parent.querySelector('.sidebar__list--submenu');
-
-    item.onclick = () => {
-        icon.classList.toggle('sidebar-year__icon--rotate');
-        list.classList.toggle('sidebar__list--submenu');
+const toogleList = () => {
+    
+    for(let item of yearLink) {
+        const parent = item.closest('.sidebar-year');
+        const icon = parent.querySelector('.sidebar-year__icon');
+        const list = parent.querySelector('.sidebar__list--submenu');
+        
+    
+        item.onclick = () => {
+            item.classList.toggle('link--active')
+            icon.classList.toggle('sidebar-year__icon--rotate');
+            list.classList.toggle('sidebar__list--submenu');
+        }
     }
+
 }
+
+const handleClickInMonth = (e) => {
+    e.preventDefault()
+    month.forEach(month => month.classList.remove('link--active'));
+    e.currentTarget.classList.add('link--active')
+}
+
+month.forEach(node => node.addEventListener('click', handleClickInMonth))
+
+
+headerBtn.addEventListener('click', toggleSidebar)
+toogleList()
+
+
